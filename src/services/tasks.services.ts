6930b12f6eb1be23e69db9117a047c49518edb55
Jena Roberts <jenaroberts@Jenas-Air.attlocal.net>
2022-06-06 13:43:29 -0400

    added my loop function and shuffle function

--- src/services/tasks.services.ts
+++ src/services/tasks.services.ts
@@ -1,18 +1,26 @@
+import { MatchKeysAndValues } from "mongodb";
+import { Key } from "readline";
+import { ClassElement } from "typescript";
 import { getDb } from "../gateway/connectDb";
+import { shuffle } from "../util";
+export interface Task {
+  name: string;
+  checked: boolean;
+}
 
 export interface TaskPlan {
-  m: string[];
-  t: string[];
-  w: string[];
-  th: string[];
-  f: string[];
-  sa: string[];
-  su: string[];
+  m: Task[];
+  t: Task[];
+  w: Task[];
+  th: Task[];
+  f: Task[];
+  sa: Task[];
+  su: Task[];
   userId: string;
   habits: string[];
   createdAt: Date;
-} 
- 
+  takesMeds: boolean;
+}
 
 export const getTasksCol = async () => {
   const db = await getDb();
@@ -26,30 +34,44 @@ export const getTasks = async (userId: string) => {
   return allTasks;
 };
 
+//your function is going to determine which tasks are done on which day at random
+//you have to figure out how to take an array of strings or tasks and turn it into an object where each day has some/none of the tasks
 // take in a string array of tasks, a user id, and a string array of habits
 // loop through the tasks and assign them a day m,t,w,etc.
 
-// export const createPlan = async (tasks: string[]) => {
-//   // const col = await getTasksCol();
-//   // const { insertedId } = await col.insertOne(task);
-//   // return insertedId;
-// };
-
-
-
-// so your function is going to determine which tasks are done on which day at random
-//you have to figure out how to take an array of strings or tasks and turn it into an object where each day has some/none of the tasks
-
-export const createPlan = async (tasks: string[], userId: string, habits: string[] ) => {
-const userTasks = await getTasks(userId)
-const { insertedId } = await userTasks.insertOne(tasks: "id") 
-const results = 
-
-return taskResults 
-
-}
-
-
-const col = getCol();
-  const snapshot = await col.orderBy("CreatedAt").get();
-  return snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+export const createPlan = async (
+  tasks: string[],
+  userId: string,
+  habits: string[],
+  takesMeds: boolean
+) => {
+  const plan: TaskPlan = {
+    m: [],
+    t: [],
+    w: [],
+    th: [],
+    f: [],
+    sa: [],
+    su: [],
+    userId,
+    habits,
+    createdAt: new Date(),
+    takesMeds,
+  };
+  const days = shuffle([
+    "m",
+    "t",
+    "w",
+    "th",
+    "f",
+    "sa",
+    "su",
+  ]) as (keyof TaskPlan)[];
+  const shuffled = shuffle(tasks);
+  let i = 0;
+  for (const task of tasks) {
+    const day = days[i];
+    (plan[day] as Task[]).push({ name: task, checked: false });
+  }
+  return plan;
+};
--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,21 @@
 //shuffle array function
-import { TaskPlan } from "./services/tasks.services";
 
-// function shuffle(array) {
-//     let currentIndex = array.length,  randomIndex;
+export const shuffle = (array: any[]) => {
+  let currentIndex = array.length,
+    randomIndex;
 
-//     // While there remain elements to shuffle.
-//     while (currentIndex != 0) {
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-//       // Pick a remaining element.
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 
-//       // And swap it with the current element.
-//       [array[currentIndex], array[randomIndex]] = [
-//         array[randomIndex], array[currentIndex]];
-//     }
-
-//     return array;
-//   }
+  return array;
+};
